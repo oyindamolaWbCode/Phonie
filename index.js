67c1ab2b583beddb86f771b0c9255067ade8037f
OyindamolaWbCode <kushimo.oyindamola@gmail.com>
2022-07-28 02:50:49 +0200

    escaping + to be read as a string and not operator

--- index.js
+++ index.js
@@ -5,17 +5,19 @@ const button = document.getElementById('submit');
 console.log(button)
 
 //using Regex
-// airtel code
-const airtelNumber = /"0802", "0808", "0812", "0701", "0708", "0902", "0907", "0901"/;
+//starting with a +
+const validNumbrPattern = /^\+/
+// // airtel code
+// const airtelNumber = /^"0802", "0808", "0812", "0701", "0708", "0902", "0907", "0901"/;
 
-//mtn code
-const mtnNumber = /"0814", "0806", "0703", "0706", "0810", "0813", "0814", "0816", "0903", "0906"/;
+// //mtn code
+// const mtnNumber = /^"0814", "0806", "0703", "0706", "0810", "0813", "0814", "0816", "0903", "0906"/;
 
-//9mobile code
-const etisalatNumber = /"0809", "0817", "0818", "0908", "0909"/;
+// //9mobile code
+// const etisalatNumber = /^"0809", "0817", "0818", "0908", "0909"/;
 
-//glo code 
-const gloNumber = /"0805", "0807", "0811", "0705", "0815", "0905", "0915"/;
+// //glo code 
+// const gloNumber = /^"0805", "0807", "0811", "0705", "0815", "0905", "0915"/;
 
 //return error when submit is clicked but no num is inputed
 
@@ -30,3 +32,7 @@ function throwAnError(e){
         return;
     }
 }
+
+typeIn.addEventListener('keyUp', (event)=>{
+    console.log(event.target.value);
+})
